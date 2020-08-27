58d3db272db5084d44effab8599ab6cce777a294
rnang0 <yangyifan0609@qq.com>
2020-08-27 22:24:32 +0800

    8.27学习动态规划基础，找零钱问题

---
+++ 23-动态规划/src/com/embedded/Main.java
@@ -0,0 +1,12 @@
+package com.embedded;
+
+/**
+ * @author rnang0
+ * @date 2020/8/27
+ **/
+public class Main {
+    public static void main(String[] args) {
+        int[] faces = {1, 5, 20, 25};
+        System.out.println(CoinChange.coins5(41, faces));
+    }
+}
